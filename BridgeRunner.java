887a60d26f49c9dd4c8cbb8a11fb3567c579c7d9
Mitch <mdhurley@pugetsound.edu>
2023-04-11 21:55:49 +0000

    initial commit

--- BridgeRunner.java
+++ BridgeRunner.java
@@ -7,7 +7,15 @@ public class BridgeRunner {
 	public static void main(String[] args) {
 
 		// TODO - check command line inputs
+		if (args.length < 3){
 
+			//TODO throw exception and give usage
+		}
+		if (Integer.parseInt(args[2])){
+
+		}
+
+		OneLaneBridge bridge = new OneLaneBridge(0);
 		// TODO - instantiate the bridge
 		
 		// TODO - allocate space for threads
---
+++ OneLaneBridge.java
@@ -0,0 +1,9 @@
+public class OneLaneBridge extends Bridge{
+    //Might have to add more variables
+    private final int MAX_CARS;
+    public boolean direction; //true vs false
+
+    public OneLaneBridge(final int MAX_CARS){
+        this.MAX_CARS = MAX_CARS;
+    }
+}
