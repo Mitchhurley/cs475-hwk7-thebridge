b1493ba75fbd9ce6a55aa20876ad7ca5113b5b80
Mitch <mdhurley@pugetsound.edu>
2023-04-11 22:37:15 +0000

    Developing Bridgerunner class

--- BridgeRunner.java
+++ BridgeRunner.java
@@ -1,30 +1,38 @@
 /**
  * Runs all threads
  */
-
 public class BridgeRunner {
 
 	public static void main(String[] args) {
 
 		// TODO - check command line inputs
-		if (args.length < 3){
+		if (args.length < 3) {
 			System.out.println("Not enough arguments, Usage: BridgeRunner <bridge limit> <num cars>");
-		
-			//TODO throw exception and give usage
-		}else{
-		if (Integer.parseInt(args[2])){
+			// TODO throw exception and give usage
+		} else {
+			if (Integer.parseInt(args[0]) > 0 && Integer.parseInt(args[1]) > 0) {
+				System.out.println("Num Cars and Bridge Limit have to be a positive number, Usage: BridgeRunner <bridge limit> <num cars>");
+			} else {
+				int bridgeLimit = Integer.parseInt(args[0]);
+				int numCars = Integer.parseInt(args[1]);
+				OneLaneBridge bridge = new OneLaneBridge(bridgeLimit);
+				// TODO - instantiate the bridge
 
-		}
+				Thread[] threads = new Thread[numCars];
 
-		OneLaneBridge bridge = new OneLaneBridge(0);
-		// TODO - instantiate the bridge
-		
-		// TODO - allocate space for threads
-
-		// TODO - start then join the threads
-
-		System.out.println("All cars have crossed!!");
+				for (int i = 0; i < numCars; i++) {
+					threads[i] = new Thread(new Car(i, bridge));
+					threads[i].start();
+				}
+				for (int i = 0; i < numCars; i++) {
+					try {
+						threads[i].join();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println("All cars have crossed!!");
+			}
 		}
 	}
-
 }
