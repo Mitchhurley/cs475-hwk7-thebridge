5305dff01f6700ef07308fb733235befab38aff6
Mitch <mdhurley@pugetsound.edu>
2023-04-12 22:02:11 +0000

    Wrote conditionals

--- OneLaneBridge.java
+++ OneLaneBridge.java
@@ -3,7 +3,7 @@ public class OneLaneBridge extends Bridge{
     private final int MAX_CARS;
     private Object cantEnter = new Object();
     private Object leaving = new Object();
-    public boolean direction; //true vs false
+
 
     public OneLaneBridge(final int MAX_CARS){
         this.MAX_CARS = MAX_CARS;
@@ -12,7 +12,7 @@ public class OneLaneBridge extends Bridge{
     public void arrive(Car car) throws InterruptedException{
         //Add in sync block
         synchronized (cantEnter){
-            while(true)//add conditions for it facing the right way, and the max size of the bridge isnt reached
+            while(direction == car.getDirection() || bridge.size() < MAX_CARS)//add conditions for it facing the right way, and the max size of the bridge isnt reached
             {
                 cantEnter.wait();//If it cant go right now, put it in the queue
             }
@@ -28,5 +28,23 @@ public class OneLaneBridge extends Bridge{
 
     public void exit(Car c){
         //add in sync block to make only one car leave at once
+        synchronized(leaving){
+            //if this isnt the first car in the list, then it notify's all
+            while(bridge.indexOf(c) != 0){
+                leaving.notifyAll();
+            }
+            //add sync block to prevent multiple edits of shared vars
+            synchronized(cantEnter){
+                //remove the car from the array list
+
+                //print a summary
+
+                //Notify those leaving 
+
+                //if there is nobody on the bridge, notify leaving
+
+                //notify cantEnter
+            }
+        }
     }
 }
